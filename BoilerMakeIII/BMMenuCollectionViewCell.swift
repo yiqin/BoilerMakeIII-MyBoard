6252376f233496121f3dada4628f8f6aa49d0a0e
xiaoyifan <xiaoyifan@uchicago.edu>
2015-10-17 02:35:50 -0500

    Basic layout parameters are set

---
+++ BoilerMakeIII/BMMenuCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BMMenuCollectionViewCell.swift
+//  BoilerMakeIII
+//
+//  Created by Yifan Xiao on 10/17/15.
+//  Copyright © 2015 Yi Qin. All rights reserved.
+//
+
+import UIKit
+
+class BMMenuCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellTextLabel: UILabel!
+}
---
+++ BoilerMakeIII/BMMenuViewController.swift
@@ -0,0 +1,64 @@
+//
+//  BMMenuViewController.swift
+//  BoilerMakeIII
+//
+//  Created by Yifan Xiao on 10/17/15.
+//  Copyright © 2015 Yi Qin. All rights reserved.
+//
+
+import UIKit
+
+class BMMenuViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    
+@IBOutlet weak var menuCollectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuCollectionView.delegate = self
+        menuCollectionView.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("BMMainMenuCell", forIndexPath: indexPath) as! BMMenuCollectionViewCell
+        return cell;
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 5;
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        let width = (self.view.frame.size.width - 22.0)/2.0
+        
+     return CGSize(width: width, height: width)
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 50.0, left: 10.0, bottom: 50.0, right: 10.0)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
