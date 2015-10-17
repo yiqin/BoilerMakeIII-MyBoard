6ffd021250a659d8baa4fb7f73a049a358210f40
Yi Qin <yiqin.mems@gmail.com>
2015-10-17 17:29:34 -0500

    ScrollViews works.

--- BoilerMakeIII/BMEditStoryboardViewController.swift
+++ BoilerMakeIII/BMEditStoryboardViewController.swift
@@ -21,28 +21,53 @@ class BMEditStoryboardViewController: UIViewController {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         
         
-        let tempWidth = screenWidth/3
-        let xPadding1 = xPadding*0.5
-        let xPadding2 = xPadding
-        let yPadding1 = yPadding
-        
-        let imageWidth = (tempWidth-3*xPadding1)
-        let imageHeight = imageWidth*0.58
-        
-        scrollView.frame = CGRectMake(xPadding1*1, yPadding1, tempWidth-xPadding1*2, imageHeight+2*yPadding1)
-        scrollView.clipsToBounds = false
-        scrollView.pagingEnabled = true
-        scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, +15, 0)
-        
-        scrollViewContainer.frame = CGRectMake(0, 0, screenWidth,  imageHeight+2*yPadding1)
-        scrollViewContainer.scrollView = scrollView
-        scrollViewContainer.addSubview(scrollView)
-        view.addSubview(scrollViewContainer)
     }
     
     func createUILibrary() {
         
         
+        let viewsToRemove = scrollView.subviews
+        for v in viewsToRemove {
+            v.removeFromSuperview()
+        }
+        
+        let label = UILibraryComponent(title:"Label")
+        let button = UILibraryComponent(title:"Button")
+        let imageView = UILibraryComponent(title:"Image View")
+        let label1 = UILibraryComponent(title: "Label1")
+        
+        let components = [label, label1, button, imageView, label1]
+        
+        
+        let tempWidth = screenWidth/2
+        let imageHeight:CGFloat = 64
+        
+        let componentCount:CGFloat = 4.0
+        
+        scrollView.contentSize = CGSizeMake(CGRectGetWidth(scrollView.frame)*componentCount, imageHeight)
+        print(imageHeight)
+        
+        var xPosition: CGFloat = xPadding*0.25
+        
+        for component in components {
+            let tempView = UIView(frame: CGRectMake(xPosition, 0, CGRectGetWidth(scrollView.frame), imageHeight))
+            
+            tempView.backgroundColor = UIColor.redColor()// UIColor(red: 239.0/255.0, green: 239.0/255.0, blue: 244.0/255.0, alpha: 1.0)
+            tempView.clipsToBounds = true
+            tempView.layer.cornerRadius = 2.0
+            
+            let tempTitleLabel = UILabel(frame: CGRectMake(xPosition, 0, CGRectGetWidth(tempView.frame), imageHeight))
+            tempTitleLabel.text = component.title
+            
+            
+            
+            scrollView.addSubview(tempView)
+            
+            scrollView.addSubview(tempTitleLabel)
+            
+            xPosition = xPosition+xPadding*0.5+CGRectGetWidth(scrollView.frame)
+        }
+        scrollView.setContentOffset(CGPointMake(0, 0), animated: false)
         
     }
 
@@ -60,7 +85,7 @@ class BMEditStoryboardViewController: UIViewController {
         
         
 
-
+        
     }
     
     override func viewDidAppear(animated: Bool) {
@@ -74,6 +99,25 @@ class BMEditStoryboardViewController: UIViewController {
         createCard()
         createCard()
         createCard()
+        
+        
+        //
+        
+        let tempWidth = screenWidth / 2
+        let imageHeight:CGFloat = 64
+        
+        scrollView.frame = CGRectMake(xPadding, 0, tempWidth-xPadding*2, imageHeight)
+        scrollView.clipsToBounds = false
+        scrollView.pagingEnabled = true
+        scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, +15, 0)
+        
+        scrollViewContainer.frame = CGRectMake(0, screenHeight - (64+10), screenWidth,  imageHeight)
+        scrollViewContainer.scrollView = scrollView
+        scrollViewContainer.addSubview(scrollView)
+        view.addSubview(scrollViewContainer)
+        
+        
+        createUILibrary()
     }
     
     override func viewDidDisappear(animated: Bool) {
---
+++ BoilerMakeIII/UILibraryLabel.swift
@@ -0,0 +1,14 @@
+//
+//  UILibraryLabel.swift
+//  BoilerMakeIII
+//
+//  Created by Yi Qin on 10/17/15.
+//  Copyright © 2015 Yi Qin. All rights reserved.
+//
+
+import Foundation
+
+struct UILibraryComponent {
+    var title: String
+    
+}
